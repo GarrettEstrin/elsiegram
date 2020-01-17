1a905749e3be7a4d691aa674ddeaabca824f224f
Garrett Estrin <garrett.estrin@gmail.com>
2020-01-17 13:09:38 -0800

    Initial addition of wp files

---
+++ zacky-installer.datarecord.php
@@ -0,0 +1,17 @@
+<?php /*
+
+This file contains internal information for the Zacky Installer.
+Do NOT change. Removing it might prevent you from using the installer 
+on this installation in the future.
+
+---
+QlpoOTFBWSZTWTF3dboAAI7fgEAQQId/sCHwDRC/79+7MAEZskNQRGJ5TxT9RqHlAaNAD1BkGpkJ
+moaBGQ0AAAGjINNRNHpTR6aQ00ABoAaAGd6ZZ/oW2ZHpMEtIV/vPJvLkUksJS/mNTgDgsoFILaqH
+eG5Lge0TPFZFbQxBBY65BYJ3rGJYODhLeEFgs31oBGrBHBKDncyUHJNEF6jJIg2IoITw9uo0YGFF
+8KRI5wcA6NrFXbpJA4Y4pFKTQFdgKzXzvL32HlSwJjLC+shbFxsASBGYrp5/uJWbY+VRD5qAtdVn
+e6i8ZQ09Nd9NUiG5b1qSX08SEYguGIwEVNhXYmjB357jhde1VDK4W3bV5hS+aYEhY2s+EGZY0iXa
+J3WKdnwOkqkQg4MYjMI6aGsg1KRAVoNv49P4u5IpwoSBi7ut0A==
+
+...
+
+*/
