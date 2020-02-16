0720660b75a91f421aea7f124b606a55bf3ec51b
Garrett Estrin <garrett.estrin@gmail.com>
2020-02-15 23:06:50 -0800

    Posts are created

--- wp-content/themes/fabulist/assets-custom/js/post.js
+++ wp-content/themes/fabulist/assets-custom/js/post.js
@@ -3,6 +3,8 @@ let target = document.getElementById("target");
 let caption = document.getElementById('jsPostCaption');
 let postSubmit = document.getElementById('jsPostSubmit');
 let label = document.getElementById("jsInputLabel");
+let form = document.getElementById("jsPostForm");
+
 let imageSelected = false;
 let captionValid = false;
 
@@ -26,12 +28,43 @@ function showImage(src,target) {
     });
 }
 
-function submitPost() {
-    if(!imageSelected && !captionValid) {
+function submitPost(event) {
+    event.preventDefault();
+
+    if(!imageSelected || !captionValid) {
         console.log("post not valid");
     } else {
-        console.log("submittingPost");
-        console.log(src.files[0], caption.value);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            let response = JSON.parse(this.responseText);
+            if(response.success) {
+                document.getElementById('content').innerHTML = `
+                    <div>
+                    <p class="post__success" style="
+                        text-align: center;
+                        font-size: 40px;
+                        line-height: 1;
+                        padding: 5px;
+                    ">Post was submitted successfully!</p>
+                        <p class="home__link" style="
+                            text-align: center;
+                            font-size: 25px;
+                        "><a href="/add-post">Add Another Post</a></p>
+                    </div>
+                `;
+            }
+        }
+        });
+
+        xhr.open("POST", "/wp-admin/admin-ajax.php?action=submit_elsiegram_post");
+        let data = new FormData();
+        data.append("caption", caption.value);
+        data.append("image", src.files[0]);
+        xhr.send(data);
     }
 }
 
@@ -41,7 +74,48 @@ function isCaptionValid() {
     }
 }
 
+/*!
+ * Serialize all form data into a query string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}   form The form to serialize
+ * @return {String}      The serialized form data
+ */
+var serialize = function (form) {
+
+	// Setup our serialized data
+	var serialized = [];
+
+	// Loop through each field in the form
+	for (var i = 0; i < form.elements.length; i++) {
+
+		var field = form.elements[i];
+
+		// Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+		if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+		// If a multi-select, get all selections
+		if (field.type === 'select-multiple') {
+			for (var n = 0; n < field.options.length; n++) {
+				if (!field.options[n].selected) continue;
+				serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[n].value));
+			}
+		}
+
+		// Convert field data to a query string
+		else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+			serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
+		}
+	}
+
+	return serialized.join('&');
+
+};
+
 //   handle showing image before upload 
 showImage(src,target);
+
+caption.addEventListener("change",isCaptionValid);
 // handle post submission 
-postSubmit.addEventListener('click', submitPost);
+postSubmit.addEventListener('click', function(event) {
+    submitPost(event);
+});
--- wp-content/themes/fabulist/page-add-post.php
+++ wp-content/themes/fabulist/page-add-post.php
@@ -18,12 +18,13 @@
         <p class="home__link"><a href="/">Visit ElsieGram</a></p>
         <p class="home__link">Add Post</p>
 
-
-        <input id="jsPostFile" type="file" accept="image/*" class="post__input"/>
-        <label id="jsInputLabel" for="jsPostFile" class="post__input-label">Choose An Image</label>
-        <img id="target" class="post__target" style="display:none"/>
-        <textarea name="caption" id="jsPostCaption" class="post__caption" placeholder="Caption" rows="6"></textarea>
-        <button id="jsPostSubmit" class="post__submit">Submit Image</button>
+        <form id="jsPostForm">
+            <input id="jsPostFile" type="file" accept="image/*" class="post__input"/>
+            <label id="jsInputLabel" for="jsPostFile" class="post__input-label">Choose An Image</label>
+            <img id="target" class="post__target" style="display:none"/>
+            <textarea name="caption" id="jsPostCaption" class="post__caption" placeholder="Caption" rows="6"></textarea>
+            <button id="jsPostSubmit" class="post__submit">Submit Image</button>
+        </form>
     </div>
     <script src="<?php echo ASSET_URL?>/js/post.js"></script>
 </body>
