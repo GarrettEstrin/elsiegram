2fd2221149c7c2054feffd51c4706e977e76fc10
Garrett Estrin <garrett.estrin@gmail.com>
2020-03-12 21:27:10 -0700

    Fixed post date and time bug

--- wp-content/themes/fabulist/helper-functions.php
+++ wp-content/themes/fabulist/helper-functions.php
@@ -26,20 +26,20 @@ function caption_date($post_date) {
     define("SECONDS_IN_AN_HOUR", 3600);
     define("SECONDS_IN_A_MINUTE", 60);
     define("SECONDS_IN_8_HOURS", SECONDS_IN_AN_HOUR * 8);
-    $date_time_post_epoch = strtotime(date('r', strtotime($post_date) - SECONDS_IN_8_HOURS));
+    $date_time_post_epoch = strtotime(date('r', strtotime($post_date)));
     $date_time_now_epoch = strtotime(date('m/d/Y h:i:s a', time()));
-    $midnight_of_today = strtotime("today");
+    $midnight_of_today = strtotime("today") - SECONDS_IN_A_DAY;
     $difference_between_now_and_post = $date_time_now_epoch - $date_time_post_epoch;
 
     if($date_time_post_epoch < $midnight_of_today) {
         $elapsedTime = round(($date_time_now_epoch - $date_time_post_epoch)/SECONDS_IN_A_DAY);
         $typeOfMeasurement = "day";
+    } else if($difference_between_now_and_post < SECONDS_IN_A_MINUTE) {
+        $elapsedTime = round(($date_time_now_epoch - $date_time_post_epoch));
+        $typeOfMeasurement = "second";
     } else if($difference_between_now_and_post < SECONDS_IN_AN_HOUR) {
         $elapsedTime = round(($date_time_now_epoch - $date_time_post_epoch)/SECONDS_IN_A_MINUTE);
         $typeOfMeasurement = "minute";
-    } else if($difference_between_now_and_post < SECONDS_IN_A_MINUTE) {
-        $elapsedTime = round(($date_time_now_epoch - $date_time_post_epoch));
-        $typeOfMeasurement = "seconds";
     } else if($difference_between_now_and_post > SECONDS_IN_AN_HOUR) {
         $elapsedTime =  round(($date_time_now_epoch - $date_time_post_epoch)/SECONDS_IN_AN_HOUR);
         $typeOfMeasurement = "hour";
